ae606f21ef7f20ef58e865072d6be349c9ea56cd
aesperer <piyrw9754@gmail.com>
2023-11-19 11:14:02 +0900

    create :: chatRoom Service

--- src/main/java/esperar/stompv2/domain/room/service/ChatRoomService.java
+++ src/main/java/esperar/stompv2/domain/room/service/ChatRoomService.java
@@ -1,9 +1,34 @@
 package esperar.stompv2.domain.room.service;
 
+import esperar.stompv2.domain.room.ChatRoom;
+import esperar.stompv2.domain.room.dto.ChatRoomDto;
+import esperar.stompv2.domain.room.repository.ChatRoomRepository;
+import esperar.stompv2.domain.user.User;
+import esperar.stompv2.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final UserRepository userRepository;
+
+    @Transactional(rollbackFor = Exception.class)
+    public Long createChatRoom(ChatRoomDto.Request request) {
+
+        User user1 = userRepository.findById(request.getUser1Id()).orElseThrow(NoSuchElementException::new);
+        User user2 = userRepository.findById(request.getUser2Id()).orElseThrow(NoSuchElementException::new);
+
+        if (user1.equals(user2)) {
+            throw new RuntimeException("유저 혼자서 방을 만들 수 없습니다.");
+        }
+
+        ChatRoom chatRoom = ChatRoom.create(request.getRoomName(), user1, user2);
+        return chatRoomRepository.save(chatRoom).getId();
+    }
 }
